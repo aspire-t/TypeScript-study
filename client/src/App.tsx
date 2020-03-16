9ebbc47f9b178c8be8837aa3e5bd9889dba2a605
aspire-t <zhijianwubu@gmail.com>
2020-03-16 19:57:24 +0800

    serve and client

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { HashRouter, Switch, Route } from 'react-router-dom'
+import LoginPage from './Pages/Login'
+import HomePage from './Pages/Home'
+
+// const Home: () => JSX.Element = () => {}
+export default () => {
+  return (
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/login" exact component={LoginPage} />
+        </Switch>
+      </HashRouter>
+    </div>
+  )
+}
--- server/README.md
+++ server/README.md
@@ -3,4 +3,3 @@
 TypeScript 学习
 
 TypeScript 结合 Express 开发后台
-TypeScript 结合 React 开发前端
